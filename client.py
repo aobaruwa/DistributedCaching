5e2aecdc5b12617a5ff0b3314f02b804e309bb03
Ahmed Baruwa <aobaruwa@yahoo.com>
2023-06-16 13:55:41 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,20 +1,57 @@
 import socket
+from client import Client
+from consistency_hashing import ConsistentHashing
+from server import Server
+import sys 
+
 class Client():
-    def __init__(self, host_port):
-        host = socket.gethostname()  # as both code is running on same pc
-        self.client_socket = socket.socket()  # instantiate
-        self.client_socket.connect((host, host_port))  # connect to the server
-        msg = input("")  # take input
+    def __init__(self, ports):
+        """
+        Client object that talks to servers using any of the ports listed in ### ports ###
+        It uses Consistency Hashing to select and retrieve messages from the distributed cache
 
-        while msg != "end":
-            self.client_socket.send(msg.encode())  # send message
-            data = self.client_socket.recv(1024).decode()  # receive response
-
-            print('Received from server: ' + data)  # show in terminal
-
-            msg = input("")  # again take input
-
-        self.client_socket.close()  # close the connection
+        input : ports - List of ports for servers listening to messages.
+        """
+        # start all servers
+        self.host = socket.gethostname()
+        ports  = [33330, 33331, 33332, 33333]
+        servers = [(self.host, p) for p in ports]
         
-if __name__=="__main__":
-  c = Client(host_port=30000)
+        self.ring = ConsistentHashing(servers)
+
+        self.client_sockets = {s : self.connect2server(s) for s in servers}
+
+
+    def connect2server(self, server):
+        client_socket = socket.socket()
+        client_socket.connect(server)
+        return client_socket
+
+
+
+    def set(self, key, value):
+        server = self.ring.get_node(key)
+        print(f"writing data to {server} ...")
+        # client_socket = socket.socket()
+        # if server not in self.connected_servers:
+        #     self.client_socket.connect(server)
+        #     self.connected_servers.add(server)
+        msg = "set " + str(key) + " " + str(value)
+        client_socket  = self.client_sockets[server]
+        client_socket.send(msg.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+        print('Received from server: ' + data)  # show in terminal
+        #print("connected servers", self.connected_servers)
+
+    def get(self, key):
+        server = self.ring.get_node(key)
+        print(f"retrieving data from {server} ...")
+        # client_socket = socket.socket()
+        # if server not in self.connected_servers:
+        #     self.client_socket.connect(server)
+        #     self.connected_servers.add(server)
+        client_socket = self.client_sockets[server]
+        msg = "get " + str(key)
+        client_socket.send(msg.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+        print('Received from server: ' + data)  # show in terminal
